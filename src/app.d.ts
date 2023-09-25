8f51790d1231a1c3d570cb8f4e324b333715e8c1
Stephen Darnell <stephen.darnell@outlook.com>
2023-09-25 12:14:39 +0100

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.3.0
      project name = cf-svelte
      framework = svelte
      framework cli = create-svelte@5.0.6
      package manager = npm@8.19.4
      wrangler = wrangler@3.9.0
      git = 2.39.2 (Apple Git-143)

---
+++ src/app.d.ts
@@ -0,0 +1,17 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+        interface Platform {
+          env: {
+            COUNTER: DurableObjectNamespace;
+          };
+          context: {
+            waitUntil(promise: Promise<any>): void;
+          };
+          caches: CacheStorage & { default: Cache }
+        }
+    }
+}
+
+export {};

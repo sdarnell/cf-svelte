0e5106e7f51ceb0624b13272c05a3b14fd46eb2c
Stephen Darnell <stephen.darnell@outlook.com>
2023-09-25 13:37:17 +0100

    Give a better error for unrecognised API requests.

---
+++ src/routes/api/[...catchall]/+server.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+
+function NotFound() {
+	throw error(404, 'API endpoint not found');
+}
+
+export {
+	NotFound as GET,
+	NotFound as POST,
+	NotFound as PUT,
+	NotFound as PATCH,
+	NotFound as DELETE,
+}

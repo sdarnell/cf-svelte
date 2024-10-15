ab7355f00ca3fad7dcb1e1c978bb5b2aa471cd5b
Stephen Darnell <stephen.darnell@outlook.com>
2024-10-15 13:17:37 +0100

    Update CF adapter that includes fix for platform not being set in certain conditions (e.g. 404's) Now all of the code in server hooks is not needed :)

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,25 +1,8 @@
 // /src/hooks.server.ts
-import { dev } from '$app/environment';
 import type { Handle } from '@sveltejs/kit';
 
-// When developing, this hook will add proxy objects to the `platform` object
-// which will emulate any bindings defined in `wrangler.toml`.
-
-let platform: App.Platform;
-
-if (dev) {
-	const { getPlatformProxy } = await import('wrangler');
-	platform = await getPlatformProxy();
-	console.log('Platform initialised for local development', platform);
-}
-
 export const handle = (async ({ event, resolve }) => {
-	if (dev && platform) {
-		event.platform = {
-			...event.platform,
-			...platform
-		};
-	}
+	// TODO: Implement hooks such as auth checks etc. here
 
 	return resolve(event);
 }) satisfies Handle;
